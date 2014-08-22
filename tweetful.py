e8d462fb7708f00071cf2c77377c4f1dcecfb86b
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-22 10:37:43 -0400

    added authorization to the tweetful app

--- tweetful.py
+++ tweetful.py
@@ -1,7 +1,13 @@
 import authorization
+import json
+import requests
+
+from urls import *
 
 def main():
-	authorization.authorize()
+	auth = authorization.authorize()
+	response = requests.get(TIMELINE_URL, auth=auth)
+	print json.dumps(response.json(), indent=4)
 
 if __name__ == "__main__":
 	main()

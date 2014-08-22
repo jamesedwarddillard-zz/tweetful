2f59684299ec0dec74942f73e8fa3cbd3bcecd39
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-22 11:01:08 -0400

    added credential storing

--- authorization.py
+++ authorization.py
@@ -6,6 +6,8 @@ from requests_oauthlib import OAuth1
 from secret import CLIENT_KEY, CLIENT_SECRET
 from urls import *
 
+import json
+
 def get_request_token():
 	"""Get a token allowing us to request user authorization"""
 	oauth = OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET)
@@ -16,7 +18,7 @@ def get_request_token():
 	request_secret = credentials.get("oauth_token_secret")[0]
 	return request_token, request_secret
 
-def get_user_autorization(request_token):
+def get_user_authorization(request_token):
 	"""
 	Redirect the user to authorize the client, and get them to give us the verification code.
 	"""
@@ -35,17 +37,33 @@ def get_access_token(request_token, request_secret, verifier):
 	response = requests.post(ACCESS_TOKEN_URL, auth=oauth)
 	credentials = urlparse.parse_qs(response.content)
 	access_token = credentials.get('oauth_token')[0]
-	access_secret = credentials('oauth_token_secret')[0]
+	access_secret = credentials.get('oauth_token_secret')[0]
 	return access_token, access_secret
+
+def store_credentials(access_token, access_secret):
+	"""Save our access credentials in a json file"""
+	with open("access.json", "w") as f:
+		json.dump({"access_token": access_token,
+					"access_secret": access_secret}, f)
+
+def get_stored_credentials():
+	""" Try to retrieve stored access credentials from a json file"""
+	with open("access.json", "r") as f:
+		credentials = json.load(f)
+		return credentials["access_token"], credentials["access_secret"]
 
 def authorize():
 	""" A complete OAuth authentication flow """
-	request_token, request_secret = get_request_token()
-	verifier = get_user_autorization(request_token)
-	access_token, access_secret = get_access_token(request_token,
-		request_secret, verifier)
+	try:
+		access_token, access_secret = get_stored_credentials()
+	except IOError:
+		request_token, request_secret = get_request_token()
+		verifier = get_user_authorization(request_token)
+		access_token, access_secret = get_access_token(request_token,
+			request_secret, verifier)
+		store_credentials(access_token, access_secret)
 
-	oauth = OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET, resource_owner_key=access_token,
+	oauth = 	OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET, resource_owner_key=access_token,
 	 resource_owner_secret=access_secret)
 
 	return oauth

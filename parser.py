d42bdf989c8e884008fb16bac104bb97dcf063dc
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-24 11:25:08 -0700

    changed the prupose of the app to rank and search:

--- parser.py
+++ parser.py
@@ -3,5 +3,15 @@ import sys
 
 def make_parser():
 	"""Constructing the command line parser"""
-	description = "A twitter app that pulls down a users timeline, searches and posts"
+	description = "A twitter app that shows a user their follower rank and saves searches"
 	parser = argparse.ArgumentParser(description=description)
+
+	subparser = parser.add_subparsers(help="Available commands")
+
+	#Subparser for the rank command
+	timeline_parser = subparser.add_parser("timeline", help = "See your timeline")
+
+
+	#Subparser for the search command
+
+

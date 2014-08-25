014694376f03f65bec8fb6a2b3bc6cb4a9cfcdca
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-25 05:49:27 -0700

    updated main function that doesnt work quite yet

--- tweetful.py
+++ tweetful.py
@@ -2,14 +2,27 @@ import authorization
 import json
 import requests
 import search
+import my_parser
+import sys
 
 from urls import *
 
+
 def main():
 	auth = authorization.authorize()
-	response = requests.get(TIMELINE_URL, auth=auth)
-	search_result = search.search('James', '*recent', 100, auth)
-	print json.dumps(search_result.json(), indent=4)
+	arg_parser = my_parser.make_parser()
+	arguments = arg_parser.parse_args(sys.argv[1:])
+	#Convert parsed arguments from Namespace to dictionary
+	arguments = vars(arguments)
+	#Remove command from the arguments dictionary and store it as a variable
+	command = arguments.pop("command")
+	print arguments
+
+	if command == "search":
+		result = search.search(**arguments)
+		print json.dumps(result.json(), indent=4)
 
 if __name__ == "__main__":
 	main()
+
+

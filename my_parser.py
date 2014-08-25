7b21c355906b226618ac6837c478089aa8bbae81
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-25 05:37:19 -0700

    created a search parser and changed the filename so its importable

---
+++ my_parser.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+
+def make_parser():
+	"""Constructing the command line parser"""
+	description = "A twitter app that searches on behalf of a user"
+	parser = argparse.ArgumentParser(description=description)
+
+	subparser = parser.add_subparsers(help="Available commands")
+
+	#Subparser for the search command
+	search_parser = subparser.add_parser("search", help = "Search Twitter for things you care about")
+	search_parser.add_argument("query", help="The term you're searching for")
+	search_parser.add_argument("result_type", default="*recent", nargs="?", help="The type of search you're performing: recent, popular or mixed")
+	search_parser.add_argument("count", default=3, nargs="?", help="The number of results your search will return")
+	search_parser.add_argument("auth", default="auth", nargs="?", help="Twitter authorization")
+	search_parser.set_defaults(command = "search")
+
+	return parser
+
+
